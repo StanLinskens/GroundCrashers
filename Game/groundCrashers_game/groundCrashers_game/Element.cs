3c7d0f0e5beb6a9d5ca96b649137a1482f36a227
stan-linskens <89661@roc-teraa.nl>
2025-05-13 11:42:49 +0200

    first commit
    heheheha

---
+++ Game/groundCrashers_game/groundCrashers_game/Element.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groundCrashers_game
+{
+    enum Elements
+    {
+        Nature,
+        Ice,
+        Toxic,
+        Fire,
+        Warer,
+        Dracobnic,
+        Earth,
+        Dark,
+        Wind,
+        Psychic,
+        Light,
+        Demonic,
+        Electric,
+        Acid,
+        Magnetic
+    }
+
+    class Element
+    {
+        string Name { get; set; }
+        string StatusEffect { get; set; }
+        string EffectDescription { get; set; }
+        Effectiveness Effectiveness { get; set; }
+    }
+}
---
+++ Game/groundCrashers_game/groundCrashers_game/Stats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace groundCrashers_game
+{
+    class Stats
+    {
+        int HP { get; set; }
+        int Attack { get; set; }
+        int Defense { get; set; }
+        int Speed { get; set; }
+
+        Stats(int hp, int attack, int defense, int speed)
+        {
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+            Speed = speed;
+        }
+    }
+}
