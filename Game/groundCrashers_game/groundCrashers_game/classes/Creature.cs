a1a7eebadb2900db4d01ec08de85caad90d0b14e
Drago-1205 <89661@roc-teraa.nl>
2025-05-16 22:47:47 +0200

    removed accesive stuf

--- Game/groundCrashers_game/groundCrashers_game/classes/Creature.cs
+++ Game/groundCrashers_game/groundCrashers_game/classes/Creature.cs
@@ -7,39 +7,6 @@ using System.Threading.Tasks;
 namespace groundCrashers_game
 {
 
-    //class Creature
-    //{
-    //    int Id { get; set; }
-    //    string Name { get; set; }
-    //    string PrimaryType { get; set; }
-    //    string Subtype { get; set; }
-    //    Stats Stats { get; set; }
-    //    string Element { get; set; }
-    //    string Ability { get; set; }
-    //    string Description { get; set; }
-
-    //    Creature(
-    //        int id,
-    //        string name,
-    //        string primaryType,
-    //        string subtype,
-    //        Stats stats,
-    //        string element,
-    //        string ability,
-    //        string description,
-    //        string scientificName)
-    //    {
-    //        Id = id;
-    //        Name = name;
-    //        PrimaryType = primaryType;
-    //        Subtype = subtype;
-    //        Stats = stats;
-    //        Element = element;
-    //        Ability = ability;
-    //        Description = description;
-    //    }
-    //}
-
     public class Creature
     {
         public int id { get; set; }
