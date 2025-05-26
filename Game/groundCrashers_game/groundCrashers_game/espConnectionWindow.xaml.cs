f7da949e65bb400e72dbe57f4743400dbeb44499
Wout Vereijken <91586@roc-teraa.nl>
2025-05-26 12:06:39 +0200

    esp connection plus window

---
+++ Game/groundCrashers_game/groundCrashers_game/espConnectionWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace groundCrashers_game
+{
+    public partial class espConnectionWindow : Window
+    {
+        private readonly HttpClient client = new HttpClient();
+        private const string esp32Url = "http://192.168.230.27";
+
+        public espConnectionWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnGetUid_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string response = await client.GetStringAsync(esp32Url);
+                TxtResult.Text = response;
+            }
+            catch (Exception ex)
+            {
+                TxtResult.Text = "connection error:\n" + ex.Message;
+            }
+        }
+    }
+}
