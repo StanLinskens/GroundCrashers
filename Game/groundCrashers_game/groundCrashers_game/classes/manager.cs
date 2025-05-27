0a0116e314c8f24384afec0a1e86696297cac1b1
pattje72 <91815@roc-teraa.nl>
2025-05-27 19:42:20 +0200

    Revert "Revert "very improtante""
    i can do that shit two huh.
    dont try me bitch

--- Game/groundCrashers_game/groundCrashers_game/classes/manager.cs
+++ Game/groundCrashers_game/groundCrashers_game/classes/manager.cs
@@ -689,8 +689,7 @@ namespace groundCrashers_game.classes
             switch (actionSplit[1])
             {
                 case "Health":
-                    creature.stats.max_hp = (int)Math.Round(creature.stats.max_hp * number);
-                    creature.stats.hp = creature.stats.max_hp; // Reset current hp to max after buff
+                    creature.stats.max_hp = (int)Math.Round(creature.stats.max_hp * number)
                     break;
                 case "Attack":
                     creature.stats.max_attack = (int)Math.Round(creature.stats.max_attack * number);
